147810df7610cd51d50f58a68bb1422da21d1539
Mozo <devnicolasmozo@gmail.com>
2021-10-19 10:12:16 +0200

    Improve refactor

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ class FizzBuzz {
 
     setValues(quantity) {
         for(let i = 1; i <= quantity; i++) {
-            const isMultipleOfFive = i % 5 === 0;
             const isMultipleOfThree = i % 3 === 0;
+            const isMultipleOfFive = i % 5 === 0;
+            const fizzWord = isMultipleOfThree ? "Fizz" : "";
+            const buzzWord = isMultipleOfFive ? "Buzz" : "";
+            const finalWord = fizzWord + buzzWord;
 
-            if(isMultipleOfThree && isMultipleOfFive) {
-                this.values.push("FizzBuzz");
-            } else if(isMultipleOfThree) {
-                this.values.push("Fizz");
-            } else if(isMultipleOfFive) {
-                this.values.push("Buzz");
+            if(finalWord) {
+                this.values.push(finalWord);
             } else {
                 this.values.push(i);
-            }           
+            }         
         }
     }
 
